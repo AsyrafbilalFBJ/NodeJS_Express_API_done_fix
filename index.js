0e32b2b4c917bdae9e61d838ae40f41624dac58a
asyrafbg <c360dsx2422@bangkit.academy>
2023-06-02 20:21:48 +0700

    fix host & port

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const router = require('./route/router')
 const cors = require('cors')
 
 const app = express()
-const port = process.env.PORT || 8000
+// const port = process.env.PORT || 8000
+// const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const HOST = process.env.HOST || '0.0.0.0'
+const PORT = process.env.PORT || 3000
 
 app.use(cors({ origin: 'null' }))
 app.use(express.json())
 app.use(router)
 
-app.listen(port, () => {
-    console.log('Server is up on port '+ port)
+app.listen(PORT, HOST,  () => {
+    console.log(`NODE_ENV=${NODE_ENV}`);
+    console.log(`App listening on http://${HOST}:${PORT}`);
 })
