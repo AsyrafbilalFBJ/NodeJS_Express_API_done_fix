b663d482644157e3952552bdff64f10e14f0f431
asyrafbg <c360dsx2422@bangkit.academy>
2023-06-02 17:52:50 +0700

    fix port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const app = express()
 const port = 8000
-const host = rocess.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
 
 app.use(cors({ origin: 'null' }))
 app.use(express.json())
