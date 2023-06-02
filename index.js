4f54fab54f749c404e7c61e2a2dc142b8ea72a2d
asyrafbg <c360dsx2422@bangkit.academy>
2023-06-02 17:52:23 +0700

    fix port

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const router = require('./route/router')
 const cors = require('cors')
 
 const app = express()
-const port = process.env.PORT || 8000
+const port = 8000
+const host = rocess.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
 
 app.use(cors({ origin: 'null' }))
 app.use(express.json())
 app.use(router)
 
-app.listen(port, process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0:', () => {
+app.listen(port,host, () => {
     console.log('Server is up on port ' + port)
 })
