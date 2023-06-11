86b2dc5fa081996851c32f08c1233f8a5fa55241
AsyrafbilalFBJ <100703987+AsyrafbilalFBJ@users.noreply.github.com>
2023-06-12 01:25:44 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: '34.101.59.151',
+    host: 'localhost',
     user: 'root',
-    password: 'nutrilife-sql2',
-    database: 'nutrilife' //isi sesuai dengan nama database
+    password: '',
+    database: 'database' //isi sesuai dengan nama database
 })
 
 module.exports = connection
